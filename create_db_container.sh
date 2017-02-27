231ba4fcfbe33a285cf2e7b91e35ab605a94b818
Gavin Brebner <gavin.brebner@hp.com>
2017-02-27 17:26:51 +0100

    now with stored procedure

--- create_db_container.sh
+++ create_db_container.sh
@@ -15,5 +15,54 @@ docker run --name ticketerdb -e MYSQL_ROOT_PASSWORD=$(cat $PASSWORD_FILE)\
                              -e MYSQL_ROOT_HOST=172.17.0.1\
                              -p 6603:3306 -d mysql/mysql-server:latest
 
+echo "`date` : waiting for the container to boot"
+while ! nc -z 172.17.0.1 6603; do
+    sleep 10
+    echo -n "."
+done
+echo "`date` : waiting over - continuing"
+
+# create the user table in the database
+mysql -uticketer -p$(cat $PASSWORD_FILE) --host=172.17.0.1 --port=6603 -DTICKETER -e 'CREATE TABLE `TICKETER`.`tbl_user` (\
+`user_id` BIGINT NOT NULL AUTO_INCREMENT,\
+`user_name` VARCHAR(45) NULL,\
+`user_username` VARCHAR(45) NULL,\
+`user_password` VARCHAR(45) NULL,\
+`user_role` VARCHAR(8) NULL,\
+PRIMARY KEY (`user_id`));'
+# add the stored procedure for creating users - note requires root priviledges
+cat > script.sql<<'EOF'
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createUser`(
+    IN p_name VARCHAR(20),
+    IN p_username VARCHAR(20),
+    IN p_password VARCHAR(20),
+    IN p_role VARCHAR(8)
+)
+BEGIN
+    IF ( select exists (select 1 from tbl_user where user_username = p_username) ) THEN
+        select "Username Exists !!";
+    ELSE
+        insert into tbl_user
+        (
+            user_name,
+            user_username,
+            user_password,
+            user_role
+        )
+        values
+        (
+            p_name,
+            p_username,
+            p_password,
+            p_role
+        );
+    END IF;
+END$$
+DELIMITER ;
+EOF
+mysql -uroot -p$(cat $PASSWORD_FILE) --host=172.17.0.1 --port=6603 -DTICKETER < script.sql
+
+
 echo "Database can now be accessed via :"
 echo "mysql -uticketer -p$(cat $PASSWORD_FILE) --host=172.17.0.1 --port=6603 -DTICKETER"
