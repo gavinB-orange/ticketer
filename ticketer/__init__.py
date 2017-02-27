f914f4f4e617d3ea8df7ea37735209830d257e10
Gavin Brebner <gavin.brebner@hp.com>
2017-02-27 18:12:50 +0100

    another pit snapshot of wip

--- ticketer/__init__.py
+++ ticketer/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 name = "ticketer"
+port = 7777
 
 app = Flask(name)
 
--- ticketer/main.py
+++ ticketer/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 from processor import Processor
 
 # fix import for running as script
@@ -11,13 +11,15 @@ if __name__ == '__main__':
         sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
         from ticketer import name as app_name
         from ticketer import app
+        from ticketer import port
     else:
         from ..ticketer import name as app_name
         from ..ticketer import app
+        from ..ticketer import port
 
 def main():
     print "***",app_name,"***"
-    app.run(port=7777, debug=True)
+    app.run(port=port, debug=True)
 
 
 @app.route("/")
@@ -28,6 +30,23 @@ def home():
 @app.route("/hi")
 def sayhi():
     return p.say("hi")
+
+
+@app.route("/showSignUp")
+def showSignUp():
+    return render_template('signup.html')
+
+
+@app.route("/signUp",methods=['POST'])
+def signUp():
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+    #validate
+    if _name and _email and _password:
+        return json.dumps({'html': '<span>All fields good!!</span>'})
+    else:
+        return json.dumps({'html': '<span>Enter the required fields</span>'})
 
 
 if __name__ == '__main__':
